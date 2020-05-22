88556c6e60dd01f3853c54a80af16c3af3f25ccd
LauraPeralta85 <lalaperalta85@gmail.com>
2020-05-22 11:35:18 -0500

    [fix] folder name

---
+++ 0x04-unity_publishing/Assets/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float RotationSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left * (RotationSpeed * Time.deltaTime));
+        //transform.Rotate(45 * (RotationSpeed * Time.deltaTime));
+    }
+}
